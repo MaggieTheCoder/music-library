7d132523cf11a1fe0e965ba7a37296025ee01e55
MaggieTheCoder <mdurandt@gmail.com>
2021-12-16 15:14:56 +0000

    added controller/routes and wrote first test

--- src/app.js
+++ src/app.js
@@ -1,5 +1,10 @@
 const express = require('express');
+const artistRouter = require('./routes/artist.js');
+
 const app = express();
+
 app.use(express.json());
+
+app.use('/artist', artistRouter);
 
 module.exports = app;
--- utils/create-database.js
+++ utils/create-database.js
@@ -1,27 +1,28 @@
-//require the promise version of mysql2
+// utils/create-database.js
+// require the promise version of mysql2
 const mysql = require('mysql2/promise');
 
-//require path to handle file paths
+// require path to handle file paths
 const path = require('path');
 
-//extract any command line arguments from argv
+// extract any command line arguments from argv
 const args = process.argv.slice(2)[0];
 
-//use args to determine if .env or env.test should be loaded
+// use args to determine if .env or .env.test should be loaded
 const envFile = args === 'test' ? '../.env.test' : '../.env';
 
-//load environment variables from env files
+// load environment variables from env files
 require('dotenv').config({
   path: path.join(__dirname, envFile),
 });
 
-//destructure environment variables from process.env
+// destructure environment variables from process.env
 const { DB_PASSWORD, DB_NAME, DB_USER, DB_HOST, DB_PORT } = process.env;
 
-//this asyncronous function will run before app
-const setupDatabase = async () => {
+// This asyncronous function will run before app
+const setUpDatabase = async () => {
   try {
-    //connect to database
+    // connect to the database
     const db = await mysql.createConnection({
       host: DB_HOST,
       user: DB_USER,
@@ -29,11 +30,11 @@ const setupDatabase = async () => {
       port: DB_PORT,
     });
 
-    //create database if it doesn't already exit
+    // create the database if it doesn't already exist
     await db.query(`CREATE DATABASE IF NOT EXISTS ${DB_NAME}`);
     db.close();
   } catch (err) {
-    //if something goes wrong, console.log the error and the current environment variables
+    // if something goes wrong, console.log the error and the current environment variables
     console.log(
       `Your environment variables might be wrong. Please double check .env file`
     );
@@ -48,5 +49,5 @@ const setupDatabase = async () => {
   }
 };
 
-//run the async function
-setupDatabase();
+// run the async function
+setUpDatabase();
